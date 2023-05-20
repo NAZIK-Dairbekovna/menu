b3542453528741ed9564f5040c4d056b2cf1a722
Nazik Dairbekova <113763454+NAZIK-Dairbekovna@users.noreply.github.com>
2023-05-21 00:29:31 +0600

    tt (#2)

--- src/components/pages/Menu.tsx
+++ src/components/pages/Menu.tsx
@@ -17,21 +17,24 @@ const Menu = () => {
                        foods.map(food => (
                            <li
                                key={food.id}
-                               className='w-[260px] h-[240px] py-2 px-2
+                               className='w-[260px] h-[230px] py-2 px-2
                             bg-gray-100 rounded-[10px] mx-4'>
                                {
-                                   <div className='flex items-center justify-between'>
+                                   <div className='p-2'>
                                        <div>
                                            <img src={food.photo} alt=""/>
-                                           <h1 className='mt-2'>{food.name}</h1>
-                                           <h1>{food.price}$</h1>
+                                           <h1 className='mt-2 font-medium'>{food.name}</h1>
                                        </div>
 
-                                       <button onClick={() => {
-                                           dispatch(Order(food))}}
-                                               className='bg-yellow-300 rounded py-1 px-3 '>
-                                           {'to order'}
-                                       </button>
+                                       <div className='flex items-center justify-between'>
+                                           <h1 className='mt-2 font-medium'>{food.price}$</h1>
+                                           <button onClick={() => {
+                                               dispatch(Order(food))}}
+                                                   className='bg-yellow-400 text-amber-50 rounded-xl py-2 px-8'>
+                                               {'to order'}
+                                           </button>
+                                       </div>
+
                                    </div>
                                }
                            </li>
